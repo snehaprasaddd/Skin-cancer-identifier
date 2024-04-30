c4d3e3b8eebe638c3bd7d63998434cf3c51ad19f
Sneha <prasadsneha47@gmail.com>
2024-04-30 14:13:58 +0530

    folder added

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+project_name="cnnClassifier"
+
+list_of_files=[
+    ".github/workflows/.gitkeep"
+    f"src/{project_name}/__intit__.py",
+    f"src/{project_name}/__components__.py",
+    f"src/{project_name}/__utils__.py",
+    f"src/{project_name}/config/__intit__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__intit__.py",
+    f"src/{project_name}/entity/__intit__.py",
+    f"src/{project_name}/constants/__intit__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml"
+    "requirements.txt",
+    "setup.py"
+    "research/trials.ipynb"
+]
+
+for filepath in  list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating Directory; {filedir} for the file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Created empty File:{filename}")
+
+    else:
+        logging.info(f"File exists")
+
